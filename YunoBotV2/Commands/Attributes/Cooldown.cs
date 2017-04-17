e8c99807edaf65358bc14d894659621a81fe1c42
MeLikeChoco <melikechoco@gmail.com>
2017-04-16 21:14:36 -0400

    made a cooldown attribute

---
+++ YunoBotV2/Commands/Attributes/Cooldown.cs
@@ -0,0 +1,94 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace YunoBotV2.Commands.Attributes
+{
+    public class Cooldown : PreconditionAttribute
+    {
+
+        private static ConcurrentDictionary<string, ConcurrentDictionary<ulong, Stopwatch>> _cooldowns = new ConcurrentDictionary<string, ConcurrentDictionary<ulong, Stopwatch>>();
+        private static Timer _cleanCooldowns = new Timer(CleanCooldowns, null, 100, 100);
+        private int _cooldown;
+
+        public Cooldown(int seconds)
+        {
+            _cooldown = seconds;
+        }
+
+        public override Task<PreconditionResult> CheckPermissions(ICommandContext context, CommandInfo command, IDependencyMap map)
+        {
+
+            if (_cooldowns.TryAdd(command.Name, new ConcurrentDictionary<ulong, Stopwatch>()))
+            {
+
+                var stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                if (_cooldowns[command.Name].TryAdd(context.Guild.Id, stopwatch))
+                {
+                    return Task.FromResult(PreconditionResult.FromSuccess());
+                }
+
+            }
+            else
+            {
+
+                if (_cooldowns[command.Name].TryGetValue(context.Guild.Id, out var stopwatch))
+                {
+
+                    if (stopwatch.Elapsed > TimeSpan.FromSeconds(_cooldown))
+                    {
+                        _cooldowns[command.Name].TryRemove(context.Guild.Id, out stopwatch);
+                        return Task.FromResult(PreconditionResult.FromSuccess());
+                    }
+                    else
+                    {
+                        var secondsLeft = TimeSpan.FromSeconds(_cooldown) - stopwatch.Elapsed;
+                        context.Channel.SendMessageAsync($"The command is on cooldown! Remaining time left: {secondsLeft.Seconds} second.");
+                        return Task.FromResult(PreconditionResult.FromError($"The command is on cooldown! Remaining time left: {secondsLeft.Seconds} second."));
+                    }
+
+                }
+                else
+                {
+                    stopwatch = new Stopwatch();
+                    stopwatch.Start();
+                    _cooldowns[command.Name].TryAdd(context.Guild.Id, stopwatch);
+                    return Task.FromResult(PreconditionResult.FromSuccess());
+                }
+
+            }
+
+            return Task.FromResult(PreconditionResult.FromSuccess());
+
+        }
+
+        private static void CleanCooldowns(object state)
+        {
+
+            foreach (var command in _cooldowns)
+            {
+
+                foreach (var kv in command.Value)
+                {
+
+                    if (kv.Value.Elapsed > TimeSpan.FromSeconds(60))
+                    {
+                        _cooldowns[command.Key].TryRemove(kv.Key, out var blah);
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+}
---
+++ YunoBotV2/Commands/Info.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord;
+using System.Collections.Concurrent;
+using YunoBotV2.Commands.Attributes;
+
+namespace YunoBotV2.Commands
+{
+    public class Info : CustomModuleBase
+    {
+
+        [Command("channel")]
+        [Summary("Get a tag cloud for a channel")]
+        [Cooldown(10)]
+        [RequireContext(ContextType.Guild)]
+        public async Task ChannelCommand(SocketTextChannel channel)
+        {
+
+            IEnumerable<IMessage> messages = await channel.GetMessagesAsync(1000).Flatten();
+            IEnumerable<string> content = messages.Select(message => message.Content);
+            var count = new ConcurrentDictionary<string, int>();
+
+            Parallel.ForEach(content, str =>
+            {
+                var array = str.Split(' ');
+
+                //im sure no message is long enough to warrant a parallel foreach
+                foreach (var token in array)
+                {
+                    count.AddOrUpdate(token, 1, (key, oldvalue) => ++oldvalue);
+                }
+            });
+
+            IEnumerable<KeyValuePair<string, int>> top10 = count.Where(kv => kv.Key.Length > 3 && kv.Key != string.Empty).OrderByDescending(kv => kv.Value).Take(10);
+
+            var organizedResults = new StringBuilder($"```Here are the top 10 words used in the channel in accord to the last {messages.Count()} messages\n\n");
+
+            foreach(var kv in top10)
+            {
+                organizedResults.AppendLine($"{kv.Key.Replace("`", "").Replace("\n", "")} >> {kv.Value} times");
+            }
+
+            organizedResults.Append("```");
+            await ReplyAsync(organizedResults.ToString());
+
+        }
+
+    }
+}
