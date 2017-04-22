e1b923a8912160571a94cb503dcba0e0738787a8
MeLikeChoco <melikechoco@gmail.com>
2017-04-22 11:24:29 -0400

    added random person command

---
+++ YunoBotV2/Deserializers/RandomPerson.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YunoBotV2.Deserializers
+{
+    public class Name
+    {
+        public string title { get; set; }
+        public string first { get; set; }
+        public string last { get; set; }
+    }
+
+    public class Location
+    {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postcode { get; set; }
+    }
+
+    public class Login
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Id
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    public class Picture
+    {
+        public string large { get; set; }
+        public string medium { get; set; }
+        public string thumbnail { get; set; }
+    }
+
+    public class RandomPerson
+    {
+        public string gender { get; set; }
+        public Name name { get; set; }
+        public Location location { get; set; }
+        public string email { get; set; }
+        public Login login { get; set; }
+        public string dob { get; set; }
+        public string registered { get; set; }
+        public string phone { get; set; }
+        [JsonProperty("cell")]
+        public string cellphone { get; set; }
+        public Id id { get; set; }
+        public Picture picture { get; set; }
+        [JsonProperty("nat")]
+        public string nationality { get; set; }
+    }
+}
